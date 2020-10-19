6282efd64e4fda730865f799cd569e0d8a62d010
JulioMike <julio.oliveira@ee.ufcg.edu.br>
2020-10-19 17:01:08 -0300

    Updates

---
+++ precache-manifest.661c3dad46b2255f45a47c94242d1f17.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d17abe430f396ab9d553658bbecb853",
+    "url": "/diagnoSiS/index.html"
+  },
+  {
+    "revision": "9970f10379a2175046a6",
+    "url": "/diagnoSiS/static/css/main.983bacc3.chunk.css"
+  },
+  {
+    "revision": "2d888a9d1eb7b4d2979a",
+    "url": "/diagnoSiS/static/js/2.1e398639.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/diagnoSiS/static/js/2.1e398639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9970f10379a2175046a6",
+    "url": "/diagnoSiS/static/js/main.e17952f6.chunk.js"
+  },
+  {
+    "revision": "3cd788549f46666ae04f",
+    "url": "/diagnoSiS/static/js/runtime-main.3ccd6e8a.js"
+  },
+  {
+    "revision": "b5aba27e96437c4b060d2a81c965f5d9",
+    "url": "/diagnoSiS/static/media/Vector 1.b5aba27e.png"
+  },
+  {
+    "revision": "3e04a24d21fa487440afac498affb9ae",
+    "url": "/diagnoSiS/static/media/fapesq.3e04a24d.png"
+  },
+  {
+    "revision": "694d2e67a9502802faf18aabd065a822",
+    "url": "/diagnoSiS/static/media/governo.694d2e67.png"
+  },
+  {
+    "revision": "72b2132510623eb49a627560bd4dfae8",
+    "url": "/diagnoSiS/static/media/icones.72b21325.png"
+  },
+  {
+    "revision": "b9fabd245337d49a5710bdd00479ac70",
+    "url": "/diagnoSiS/static/media/logo.b9fabd24.png"
+  },
+  {
+    "revision": "9c7a1c0c96b9a22ae48dafa4aeae9907",
+    "url": "/diagnoSiS/static/media/titulo.9c7a1c0c.png"
+  }
+]);
